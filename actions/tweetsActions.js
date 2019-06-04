56995ecce785efeec44563e262797fbe8f60ecd5
salviolorenzoWela <salviolorenzo@gmail.com>
2019-06-03 20:16:29 -0400

    editing

---
+++ actions/tweetsActions.js
@@ -0,0 +1,16 @@
+import fetch from 'fetch';
+import { connect } from 'react-redux';
+
+export function fetchTweets(){
+  return function(dispatch){
+    dispatch({type: "FETCH_TWEETS"});
+    fetch(("https://jsonplaceholder.typicode.com/posts").then(response => {
+      dispatch({
+        type: "FETCH_TWEETS_FULFILLED",
+        payload: response.data
+      });
+    }).catch(err => {
+      dispatch({ type: "FETCH_TWEETS_REJECTED", payload: err});
+    })
+  }
+}
